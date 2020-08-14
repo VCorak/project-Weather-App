1e73ca205f489fa4cc925bb3b4dcccf743ac17ec
VCorak <68239365+VCorak@users.noreply.github.com>
2020-08-14 17:35:57 +0200

    Trying to display forecast with #update button

--- src/script.js
+++ src/script.js
@@ -111,17 +111,19 @@ function instantLocation(position) {
   let lon = position.coords.longitude;
   let apiKey = "21d207d4e5449385a0586090096515c7";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-
   axios.get(apiUrl).then(showCityWeather);
-  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayHourForecast);
 }
 
 function getCurrentLocation() {
   navigator.geolocation.getCurrentPosition(instantLocation);
 }
+
 let button = document.querySelector("#update-button");
 button.addEventListener("click", getCurrentLocation);
+
+
+
+
 
 //celsius to fahrenheit
 
